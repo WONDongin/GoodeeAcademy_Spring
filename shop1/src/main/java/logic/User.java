46ecbc742f7f43ce2d2e683dabb8b92e89c15029
WONDongin <88768560+WONDongin@users.noreply.github.com>
2025-06-05 16:05:16 +0900

    spring_loginmypageException
    사용자등록/로그인/마이페이지/예외처리(메세지)

---
+++ shop1/src/main/java/logic/User.java
@@ -0,0 +1,33 @@
+package logic;
+
+import java.sql.Date;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Size;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import lombok.Data;
+
+@Data
+public class User {
+	@Size(min=3, max=10, message="아이디는 3자이상 10자 이하로 입력하세요")
+	private String userid;
+	private String channel;
+	@Size(min=3, max=10, message="비밀번호는 3자이상 10자 이하로 입력하세요")
+	private String password;
+	@NotEmpty(message="사용자 이름은 필수 입니다")
+	private String username;
+	private String phoneno;
+	private String postcode;
+	private String address;
+	@NotEmpty(message="email을 입력하세요")
+	@Email(message="email 형식으로 입력하세요")
+	private String email;
+	// @NotNull(message="생일을 입력하세요") 필수입력
+	@Past(message="생일은 과거 날짜만 가능합니다")
+	@DateTimeFormat(pattern="yyyy-MM-dd")
+	private Date birthday;
+}
